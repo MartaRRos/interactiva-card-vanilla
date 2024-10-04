64af8e393a2216b298d98e0f3c535ad2c6fefbe7
mrr24699 <martarodriguezros24@alumnostrazos.net>
2024-10-04 10:42:45 +0200

    Card (sin terminar)

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,10 @@
 // El styles lo importamos aquí, ya se carga después al compilar todo
 import '../scss/styles.scss';
+
+const formElement = document.getElementById('form');
+
+const validateForm = event => {
+  event.preventDefault();
+};
+
+formElement.addEventListener('submit', validateForm);
